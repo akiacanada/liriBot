2df3127bde35f40fd9c0e071aaf859f6943e4091
Akia Canada <akiacanada@yahoo.com>
2017-06-25 02:12:12 -0500

    Second commit

---
+++ keys.js
@@ -0,0 +1,20 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'iBH5oQRgfJRIDvzjE6czYa47S',
+  consumer_secret: '<LSA9CLjyNaSmrVhAk4NnT8eqkpTGERQCJfht6G9YHqWA3ST3wZ>',
+  access_token_key: '878835930399899649-CvOgIOrLsDbB7gTc4LQ8gPJ1WsnEo4s',
+  access_token_secret: 'ZnomEBpSyz8ZyFso7828w5dIR2fJT2uifKZeraWp3iRuM',
+}
+
+
+Consumer Key (API Key)	iBH5oQRgfJRIDvzjE6czYa47S
+Consumer Secret (API Secret)	LSA9CLjyNaSmrVhAk4NnT8eqkpTGERQCJfht6G9YHqWA3ST3wZ
+Access Level	Read and write (modify app permissions)
+Owner	OroroWayne
+Owner ID	878835930399899649
+Access Token	878835930399899649-CvOgIOrLsDbB7gTc4LQ8gPJ1WsnEo4s
+Access Token Secret	ZnomEBpSyz8ZyFso7828w5dIR2fJT2uifKZeraWp3iRuM
+Access Level	Read and write
+Owner	OroroWayne
+Owner ID	878835930399899649
